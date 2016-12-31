c2a87ed483da358c8f4a18a3d4b9e9e777071ccd
Marcelo H. Carneiro <marcelo@devhaus.ie>
2016-12-31 04:16:50 +0000

    Children component example

---
+++ components/AnotherComponent.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div>
+        <p v-text="msg"></p>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg : 'Children component from math'
+        }
+    }
+}
+</script>
--- components/MathSumComponent.vue
+++ components/MathSumComponent.vue
@@ -1,26 +1,29 @@
 <template>
-  <div>
-     <input type="number" v-model="nrone" placeholder="Number one">
-     <input type="number" v-model="nrtwo" placeholder="Number two">
+    <div>
+        <another-one></another-one>
+        <hr>
+        <input type="number" v-model="nrone" placeholder="Number one">
+        <input type="number" v-model="nrtwo" placeholder="Number two">
 
-     result : <span v-text="getResult"></span>
+        result : <span v-text="getResult"></span>
 
-    <hr>
-    <button
+        <hr>
+        <button
         type="button"
         name="button"
         @click="doTheSum">Sum</button>
 
-    <button
+        <button
         type="button"
         name="button"
         @click="sumOneMoreTime">Sum same value one more time</button>
-  </div>
+    </div>
 </template>
 
 <script>
 
 import { mapState, mapActions } from 'vuex';
+import AnotherONe from "./AnotherComponent.vue";
 
 export default {
     data () {
@@ -50,6 +53,10 @@ export default {
         sumOneMoreTime () {
             this.$store.getters.sumAgainWithResult;
         }
+    },
+
+    components : {
+        'another-one' : AnotherONe
     }
 
 }
